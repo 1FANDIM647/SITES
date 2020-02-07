d7184cd6f3b5ab485d598a322fc8edf337a3a974
Michael Elkin <55627341+1FANDIM647@users.noreply.github.com>
2020-02-07 15:37:40 +0300

    Add files via upload

--- witcher tv show/js/main.js
+++ witcher tv show/js/main.js
@@ -6,3 +6,10 @@ var mySwiper = new Swiper('.swiper-container', {
      
     },
 });
+
+var menuButton = document.querySelector('menu-button');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click',function(){
+      menuButton.classList.toggle('menu-button-active');
+      menuButton.classList.toggle('header-active');
+});
